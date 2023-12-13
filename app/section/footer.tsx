2e7dee336a0b48b6e414e2afcf0e4a9a6dd07e1a
woaitsAryan <yoprince.ab@gmail.com>
2023-12-14 01:20:21 +0530

    fix: footer fixes

--- app/section/footer.tsx
+++ app/section/footer.tsx
@@ -3,7 +3,7 @@ import SocialsLink from "../components/socialsLink";
 const Footer = () => {
     return (
         <aside>
-            <ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-600 dark:text-neutral-300">
+            <ul className="flex flex-row flex-wrap md:flex-row mt-8 space-x-4 space-y-0 md:space-x-4 md:space-y-0 font-sm text-neutral-600 dark:text-neutral-300">
                 <SocialsLink url="https://www.linkedin.com/in/aryanbharti/" title="Linkedin" />
                 <SocialsLink url="mailto:workaryanbharti@gmail.com" title="Email" />
                 <SocialsLink url="https://github.com/woaitsAryan" title="Github" />
