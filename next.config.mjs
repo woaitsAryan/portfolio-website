6e85474af896b5abf1f0afd4968241fb5e1fa495
woaitsAryan <yoprince.ab@gmail.com>
2024-07-28 14:03:55 +0530

    feat: added resume in /public

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
-import million from 'million/compiler';
- 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
  
-const millionConfig = {
-  auto: true,
-  auto: { rsc: true },
-}
  
-export default million.next(nextConfig, millionConfig);
+export default nextConfig
